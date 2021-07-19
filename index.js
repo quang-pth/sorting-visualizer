854a8d5451919b2f2d331a842853246726d61e07
QuangPham <quang20201@gmail.com>
2021-07-19 17:31:57 +0700

    fix logic Selection Sort algo

--- index.js
+++ index.js
@@ -20,40 +20,40 @@ function generatebars(num = 20) {
 }
 
 // define selection sort algorithm
-async function SelectionSort(delay = 300) {
+async function SelectionSort(delay = 50) {
     let bars = document.querySelectorAll(".bar");
-    let min_idx = 0;
 
     for (let i = 0; i < bars.length; i++) {
-        min_idx = i;
+        let min_idx = i;
         // provide darkblue color for ith bar
         bars[i].style.backgroundColor = 'darkblue';
-        for (let j = i + 1; j < bar.length; j++) {
+        for (let j = i + 1; j < bars.length; j++) {
             bars[j].style.backgroundColor = 'red';
             // pause execution to generate visualization
             await new Promise((resole) => {
                 setTimeout(() => {
                     resole();
-                }, 300);
+                }, delay);
             });
             // 
             let val1 = parseInt(bars[j].childNodes[0].innerHTML);
-            let val2= parseInt(bars[min_idx].childNodes[0].innerHTML);
+            let val2 = parseInt(bars[min_idx].childNodes[0].innerHTML);
             
             // Compare val1 & val2
             if (val1 < val2) {
                 if (min_idx !== i) {
-                // Provide skyblue color to the (min-idx)th bar
-                bars[min_idx].style.backgroundColor = "rgb(24, 190, 255)";
+                    // Provide skyblue color to the (min-idx)th bar
+                    bars[min_idx].style.backgroundColor = "rgb(24, 190, 255)";
                 }
                 min_idx = j;
             } else {
                 // Provide skyblue color to the jth bar
                 bars[j].style.backgroundColor = "rgb(24, 190, 255)";
             }
-
+        }
             // To swap ith and (min_idx)th bar
             var temp1 = bars[min_idx].style.height;
+            // min_idx value label
             var temp2 = bars[min_idx].childNodes[0].innerText;
             // swap actual height
             bars[min_idx].style.height = bars[i].style.height;
@@ -65,7 +65,7 @@ async function SelectionSort(delay = 300) {
             await new Promise((resolve) => {
                 setTimeout(() => {
                     resolve();
-                }, 300);
+                }, delay);
             });
 
             // Provide skyblue color to the (min-idx)th bar
@@ -80,8 +80,6 @@ async function SelectionSort(delay = 300) {
         // To enable the button "Selection Sort" after final(sorted)
         document.getElementById("Button2").disabled = false;
         document.getElementById("Button2").style.backgroundColor = "#6f459e";
-    }
-
 }
 
 // create initial bar
@@ -90,4 +88,17 @@ generatebars();
 // re-create bars everytime user click new array button
 function generate() {
     window.location.reload();
+}
+
+
+//  function to disable the button
+function disable()
+{
+  // To disable the button "Generate New Array"
+  document.getElementById("Button1").disabled = true;
+  document.getElementById("Button1").style.backgroundColor = "#d8b6ff";
+  
+  // To disable the button "Selection Sort"
+  document.getElementById("Button2").disabled = true;
+  document.getElementById("Button2").style.backgroundColor = "#d8b6ff";  
 }
