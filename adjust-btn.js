d766a9ef2e32fffbe64ea5539eb9a5c11cf500b0
QuangPham <quang20201@gmail.com>
2021-07-20 16:31:14 +0700

    modify to affect all sorting buttons

--- adjust-btn.js
+++ adjust-btn.js
@@ -6,8 +6,11 @@ function disable()
   document.getElementById("new-arr-btn").style.backgroundColor = "#d8b6ff";
   
   // To disable the button "Selection Sort"
-  document.getElementById("selection-btn").disabled = true;
-  document.getElementById("selection-btn").style.backgroundColor = "#d8b6ff";  
+  const btnToDisable = document.querySelectorAll('.sorting-btn')
+  for (const btn of btnToDisable) {
+    btn.disabled = true;
+    btn.style.backgroundColor = "#d8b6ff";  
+  }
 }
 
 // function to enable the button
@@ -15,7 +18,10 @@ function enable() {
     // To enable the button "Generate New Array" after final(sorted)
     document.getElementById('new-arr-btn').disabled = false;
     document.getElementById('new-arr-btn').style.backgroundColor = "#6f459e";
-    // To enable the button "Selection Sort" after final(sorted)
-    document.getElementById("selection-btn").disabled = false;
-    document.getElementById("selection-btn").style.backgroundColor = "#6f459e";
+  // To enable the button "Selection Sort"
+  const btnToEnable = document.querySelectorAll('.sorting-btn')
+  for (const btn of btnToEnable) {
+    btn.disabled = false;
+    btn.style.backgroundColor = "#6f459e";  
+  }
 }
