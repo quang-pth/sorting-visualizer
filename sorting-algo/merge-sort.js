bf06a9b2803b702110438581602d6cd4a19cc472
QuangPham <quang20201@gmail.com>
2021-07-31 14:00:46 +0700

    complete animation

--- sorting-algo/merge-sort.js
+++ sorting-algo/merge-sort.js
@@ -1,59 +1,122 @@
-async function mergeSort(array) {
-  if (array. length === 1) return;
-  await runMergeSort(array, 0, array.length - 1);
-  return array;
+async function mergeSort(delay) {
+  const bars = document.querySelectorAll(".bar");
+  if (bars.length === 1) return;
+  await runMergeSort(bars, 0, bars.length - 1, delay);
+  enable();
 }
 
-function runMergeSort(array, startIdx, endIdx) {
+async function runMergeSort(bars, startIdx, endIdx, delay) {
   if (startIdx >= endIdx) return;
   const middleIdx = Math.floor((endIdx + startIdx) / 2);
-
-  await runMergeSort(array, startIdx, middleIdx);
-  await runMergeSort(array, middleIdx + 1, endIdx);
-  await mergeSortedArray(array, startIdx, middleIdx, endIdx);
+  await runMergeSort(bars, startIdx, middleIdx, delay);
+  await runMergeSort(bars, middleIdx + 1, endIdx, delay);
+  await mergeSortedArray(bars, startIdx, middleIdx, endIdx, delay);
 }
 
+async function mergeSortedArray(bars, leftArrIdx, middleIdx, endIdx, delay) {
+  const leftHalf = [];
+  const rightHalf = [];
 
-// 0 1 2 3 4
-// 1 4 5 2 6
-async function mergeSortedArray(array, leftArrIdx, middleIdx, endIdx) {
-  const leftHalf = array.slice(leftArrIdx, middleIdx + 1);
-  const rightHalf = array.slice(middleIdx + 1, endIdx + 1);
+  for (let idx = leftArrIdx; idx < middleIdx + 1; idx++) {
+    leftHalf.push(parseInt(bars[idx].style.height));
+  }
+  for (let idx = middleIdx + 1; idx < endIdx + 1; idx++) {
+    rightHalf.push(parseInt(bars[idx].style.height));
+  }
+
   const sortedArray = new Array(leftHalf.length + rightHalf.length);
   let currentIdx = 0;
   let sourceArrIdx = leftArrIdx;
+  let rightArrIdx = middleIdx + 1;
   let leftIdx = 0;
   let rightIdx = 0;
-  // let currentArrIdx = leftArrIdx;
-  
+
   while (leftIdx <= leftHalf.length - 1 && rightIdx <= rightHalf.length - 1) {
-    if (leftHalf[leftIdx] <= rightHalf[rightIdx]) {
-      sortedArray[currentIdx] = leftHalf[leftIdx];
+    // set color
+    bars[leftArrIdx].style.backgroundColor = "darkblue";
+    bars[rightArrIdx].style.backgroundColor = "darkblue";
+
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, delay);
+    });
+
+    const leftHeight = leftHalf[leftIdx];
+    const rightHeight = rightHalf[rightIdx];
+
+    if (leftHeight <= rightHeight) {
+      sortedArray[currentIdx] = leftHeight;
+      bars[sourceArrIdx].style.height = `${leftHeight}px`;
+      bars[sourceArrIdx].childNodes[0].innerHTML = leftHeight / 3;
+      bars[leftArrIdx].style.backgroundColor = "rgb(24, 190, 255)";
+
       leftIdx += 1;
+      leftArrIdx += 1;
     } else {
-      sortedArray[currentIdx] = rightHalf[rightIdx];
+      sortedArray[currentIdx] = rightHeight;
+      bars[sourceArrIdx].style.height = `${rightHeight}px`;
+      bars[sourceArrIdx].childNodes[0].innerHTML = rightHeight / 3;
+      bars[rightArrIdx].style.backgroundColor = "rgb(24, 190, 255)";
+
       rightIdx += 1;
+      rightArrIdx += 1;
     }
-    array[sourceArrIdx] = sortedArray[currentIdx];
+
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, delay);
+    });
+
+    if (leftHeight <= rightHeight) {
+      bars[rightArrIdx].style.backgroundColor = "rgb(24, 190, 255)";
+    } else {
+      bars[leftArrIdx].style.backgroundColor = "rgb(24, 190, 255)";
+    }
+
     currentIdx += 1;
     sourceArrIdx += 1;
   }
+
+  bars[sourceArrIdx].style.backgroundColor = "darkblue";
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, delay);
+  });
+
   while (leftIdx < leftHalf.length) {
     sortedArray[currentIdx] = leftHalf[leftIdx];
-    array[sourceArrIdx] = sortedArray[currentIdx];
+    bars[sourceArrIdx].style.height = `${leftHalf[leftIdx]}px`;
+    bars[sourceArrIdx].childNodes[0].innerHTML = leftHalf[leftIdx] / 3;
+
     leftIdx += 1;
     currentIdx += 1;
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, delay);
+    });
+
+    bars[sourceArrIdx].style.backgroundColor = "rgb(24, 190, 255)";
     sourceArrIdx += 1;
   }
+
   while (rightIdx < rightHalf.length) {
     sortedArray[currentIdx] = rightHalf[rightIdx];
-    array[sourceArrIdx] = sortedArray[currentIdx];
+    bars[sourceArrIdx].style.height = `${rightHalf[leftIdx]}px`;
+    bars[sourceArrIdx].childNodes[0].innerHTML = rightHalf[rightIdx] / 3;
+
     rightIdx += 1;
     currentIdx += 1;
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, delay);
+    });
+
+    bars[sourceArrIdx].style.backgroundColor = "rgb(24, 190, 255)";
     sourceArrIdx += 1;
+  }
 }
-
-
-}
-
-console.log(mergeSort([4, 3, 2, 1, 0, 0, 2, 0, -1 , -1, 0, 4]));
