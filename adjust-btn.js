f39b151cdad4ab4d1a5acb191370b5a91482c789
QuangPham <quang20201@gmail.com>
2021-08-01 13:18:03 +0700

    add disable range input

--- adjust-btn.js
+++ adjust-btn.js
@@ -1,27 +1,29 @@
 //  function to disable the button
-function disable()
-{
+function disable() {
   // To disable the button "Generate New Array"
   document.getElementById("new-arr-btn").disabled = true;
   document.getElementById("new-arr-btn").style.backgroundColor = "#d8b6ff";
-  
+
   // To disable the button "Selection Sort"
-  const btnToDisable = document.querySelectorAll('.sorting-btn')
+  const btnToDisable = document.querySelectorAll(".sorting-btn");
   for (const btn of btnToDisable) {
     btn.disabled = true;
-    btn.style.backgroundColor = "#d8b6ff";  
+    btn.style.backgroundColor = "#d8b6ff";
   }
+
+  document.getElementById("sort-speed").disabled = true;
 }
 
 // function to enable the button
 function enable() {
-    // To enable the button "Generate New Array" after final(sorted)
-    document.getElementById('new-arr-btn').disabled = false;
-    document.getElementById('new-arr-btn').style.backgroundColor = "#6f459e";
+  // To enable the button "Generate New Array" after final(sorted)
+  document.getElementById("new-arr-btn").disabled = false;
+  document.getElementById("new-arr-btn").style.backgroundColor = "#6f459e";
   // To enable the button "Selection Sort"
-  const btnToEnable = document.querySelectorAll('.sorting-btn')
+  const btnToEnable = document.querySelectorAll(".sorting-btn");
   for (const btn of btnToEnable) {
     btn.disabled = false;
-    btn.style.backgroundColor = "#6f459e";  
+    btn.style.backgroundColor = "#6f459e";
   }
+  document.getElementById("sort-speed").disabled = false;
 }
