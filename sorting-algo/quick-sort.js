6f4294c445e09c898fcf78d1913b5259a2fbb494
QuangPham <quang20201@gmail.com>
2021-08-01 13:45:51 +0700

    add complete animation

--- sorting-algo/quick-sort.js
+++ sorting-algo/quick-sort.js
@@ -5,8 +5,9 @@ async function quickSort(delay) {
 }
 
 async function quickSortPartitioning(bars, startIdx, endIdx, delay) {
-    if (startIdx >= endIdx) return;
-
+    if (startIdx >= endIdx) {
+        return;
+    }
     const pivotIdx = startIdx;
     let leftIdx = startIdx + 1;
     let rightIdx = endIdx;
@@ -15,46 +16,54 @@ async function quickSortPartitioning(bars, startIdx, endIdx, delay) {
 
     let pivotValue = parseInt(bars[pivotIdx].childNodes[0].innerHTML);
     while (leftIdx <= rightIdx) {
-        bars[leftIdx].style.backgroundColor = 'red';
-        bars[rightIdx].style.backgroundColor = 'red';
-        let leftValue = parseInt(bars[leftIdx].childNodes[0].innerHTML);
-        let rightValue = parseInt(bars[rightIdx].childNodes[0].innerHTML);
-        
+      bars[leftIdx].style.backgroundColor = "red";
+      bars[rightIdx].style.backgroundColor = "red";
+      let leftValue = parseInt(bars[leftIdx].childNodes[0].innerHTML);
+      let rightValue = parseInt(bars[rightIdx].childNodes[0].innerHTML);
 
-        await new Promise((resolve) => {
-            setTimeout(() => {
-                resolve();
-            }, delay);
-        });
-        if (leftValue > pivotValue && rightValue < pivotValue) {
-            swapValue(bars, leftIdx, rightIdx);
-        }
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, delay);
+      });
+      if (leftValue > pivotValue && rightValue < pivotValue) {
+        swapValue(bars, leftIdx, rightIdx);
+      }
 
-        // set left, right back to old color
-        bars[leftIdx].style.backgroundColor = 'rgb(24, 190, 255)';
-        bars[rightIdx].style.backgroundColor = 'rgb(24, 190, 255)';
+      // set left, right back to old color
+      bars[leftIdx].style.backgroundColor = "rgb(24, 190, 255)";
+      bars[rightIdx].style.backgroundColor = "rgb(24, 190, 255)";
 
-        if (leftValue <= pivotValue) {
-            leftIdx += 1;
-        }
+      if (leftValue <= pivotValue) {
+        leftIdx += 1;
+      }
 
-        if (rightValue >= pivotValue) {
-            rightIdx -= 1;
-        }
+      if (rightValue >= pivotValue) {
+        rightIdx -= 1;
+      }
     }
 
     await new Promise((resolve) => {
-        setTimeout(() => {
-            resolve();
-        }, delay);
+      setTimeout(() => {
+        resolve();
+      }, delay);
     });
 
     swapValue(bars, pivotIdx, rightIdx);
-  
+    
     await quickSortPartitioning(bars, startIdx, rightIdx - 1, delay);
     await quickSortPartitioning(bars, rightIdx + 1, endIdx, delay);
 
-    bars[pivotIdx].style.backgroundColor = 'rgb(24, 190, 255)';
+    await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, delay);
+      });
+    bars[pivotIdx].style.backgroundColor = "rgb(49, 226, 13)";
+    for (let idx = startIdx ? startIdx - 1 : startIdx; idx <= endIdx; idx++) {
+        bars[idx].style.backgroundColor = "rgb(49, 226, 13)";
+    }
+      
 }
 
 function swapValue(bars, i, j) {
