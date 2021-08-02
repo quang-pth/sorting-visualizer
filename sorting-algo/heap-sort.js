e77bd9c475bc73ffa2d0eccda6d76484307937aa
QuangPham <quang20201@gmail.com>
2021-08-02 12:50:13 +0700

    complete sort animation

--- sorting-algo/heap-sort.js
+++ sorting-algo/heap-sort.js
@@ -1,43 +1,88 @@
-function heapSort(array) {
-    buildMaxHeap(array);
-    for (let idx = array.length - 1; idx >= 0; idx--) {
-        swapValue(array, 0, idx);
-        siftDown(array, 0, idx - 1);
-    }
-    return array;
+async function heapSort(delay) {
+  const bars = document.querySelectorAll(".bar");
+  await buildMaxHeap(bars, delay);
+
+  for (let idx = bars.length - 1; idx >= 0; idx--) {
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, delay);
+    });
+    swapValue(bars, 0, idx);
+    await siftDown(bars, 0, idx - 1, delay);
+    bars[idx].style.backgroundColor = 'rgb(49, 226, 13)';
+  }
+
+  enable();
 }
 
-function buildMaxHeap(array) {
-    const firstParentIdx = Math.floor((array.length - 1) / 2);
-    for (let currentIdx = firstParentIdx; currentIdx >= 0; currentIdx--) {
-        siftDown(array, currentIdx, array.length - 1);
-    }
+async function buildMaxHeap(bars, delay) {
+  const firstParentIdx = Math.floor((bars.length - 1) / 2);
+  for (let currentIdx = firstParentIdx; currentIdx >= 0; currentIdx--) {
+    await siftDown(bars, currentIdx, bars.length - 1, delay);
+  }
 }
 
-function siftDown(heap, currentIdx, endIdx) {
-    let childOneIdx = currentIdx * 2 + 1;
-    while (childOneIdx <= endIdx) {
-        const childTwoIdx = currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
-        let idxToSwap = null;
+async function siftDown(heapBars, currentIdx, endIdx, delay) {
+  let childOneIdx = currentIdx * 2 + 1;
 
-        if (childOneIdx !== -1 && heap[childOneIdx] < heap[childTwoIdx]) {
-            idxToSwap = childTwoIdx;
-        } else {
-            idxToSwap = childOneIdx;
-        }
+  while (childOneIdx <= endIdx) {
+    const childTwoIdx = currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
 
-        if (heap[currentIdx] < heap[idxToSwap]) {
-            swapValue(heap, currentIdx, idxToSwap);
-            currentIdx = idxToSwap;
-            childOneIdx = currentIdx * 2 + 1;
-        } else {
-            return;
-        }
+    let idxToSwap = null;
+    if (childOneIdx !== -1 && parseInt(heapBars[childOneIdx].style.height) < (childTwoIdx !== -1 ? parseInt(heapBars[childTwoIdx].style.height) : Infinity)) {
+      idxToSwap = childTwoIdx;
+    } else {
+      idxToSwap = childOneIdx;
     }
+
+    if (idxToSwap !== -1) {
+      heapBars[idxToSwap].style.backgroundColor = "darkblue";
+    }
+    heapBars[currentIdx].style.backgroundColor = "darkblue";
+
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, delay);
+    });
+
+    if (parseInt(heapBars[currentIdx].style.height) < (idxToSwap !== -1 ? parseInt(heapBars[idxToSwap].style.height) : -Infinity)) {
+      swapValue(heapBars, currentIdx, idxToSwap);
+
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, delay);
+      });
+
+      if (idxToSwap !== -1) {
+        heapBars[idxToSwap].style.backgroundColor = "rgb(24, 190, 255)";
+      }
+      heapBars[currentIdx].style.backgroundColor = "rgb(24, 190, 255)";
+
+      currentIdx = idxToSwap;
+      childOneIdx = currentIdx * 2 + 1;
+    } else {
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, delay);
+      });
+      if (idxToSwap !== -1) {
+        heapBars[idxToSwap].style.backgroundColor = "rgb(24, 190, 255)";
+      }
+      heapBars[currentIdx].style.backgroundColor = "rgb(24, 190, 255)";
+      return;
+    }
+  }
 }
 
-function swapValue(array, i, j) {
-    const temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
+function swapValue(bars, i, j) {
+  const temp1 = bars[j].style.height;
+  const temp2 = bars[j].childNodes[0].innerText;
+  bars[j].style.height = bars[i].style.height;
+  bars[i].style.height = temp1;
+  bars[j].childNodes[0].innerText = bars[i].childNodes[0].innerText;
+  bars[i].childNodes[0].innerText = temp2;
 }
